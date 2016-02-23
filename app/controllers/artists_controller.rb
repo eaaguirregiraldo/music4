7e5a40f832bd3f89f727384514da237e802aa23d
eaaguirregiraldo <eaaguirregiraldo@gmail.com>
2016-02-23 16:27:49 -0500

    Version final

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,6 +3,8 @@ class ArtistsController < ApplicationController
     @artists = Artist.all  	
   end
   def show
-  	@albums  = Album.where("artists_id = ?", params[:id])  	
+  	@albums   = Album.where("artists_id = ?", params[:id])  	
+  	@artists  = Artist.find(params[:id])
+  	@cant_albums = Album.select("count(*) as cant_albums").where("artists_id = ?", params[:id])  	  	
   end
 end
